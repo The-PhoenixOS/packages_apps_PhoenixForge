0a47dfa364508a12132424a4cb21594422396612
darkbeast69 <guptaaryan189@gmail.com>
2022-05-18 20:00:26 +0530

    FlareZone: Init Our Feature Extension
    BlazeHouse: Adapt it for ProjectBlaze
    
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    
    BlazeHouse: Import BasePreferenceController and DashboardFragment from
    Krypton
    
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    
    FlareZone: Adapt for ProjectFlare
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

---
+++ src/com/flare/zone/navigation/IBubbleNavigation.java
@@ -0,0 +1,15 @@
+package com.flare.zone.navigation;
+
+import android.graphics.Typeface;
+import com.flare.zone.navigation.BubbleNavigationChangeListener;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+}
